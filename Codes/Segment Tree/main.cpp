da771a4d78b4a12fd8be12fe7c98e5d9bde3b444
Ebrahim Emad <ebemad8@gmail.com>
2023-11-28 09:35:34 +0200

    My changes

--- Codes/Segment Tree/main.cpp
+++ Codes/Segment Tree/main.cpp
@@ -12,6 +12,22 @@ void const_tree(vector<int>a,vector<int>&seg,int low,int high,int pos){
     seg[pos]=min(seg[2*pos+1],seg[2*pos+2]);
 
 }
+void func(int a[],int seg[],int l,int h,int pos){
+    if(l==h){
+        seg[pos]=a[l];
+        return ;
+    }
+    ll m=(l+h)/2;
+    func(a,seg,l,m,2*pos+1);
+    func(a,seg,m+1,h,2*pos+2);
+    seg[pos]=min(seg[2*pos+1],seg[2*pos+2]);
+}
+int get_ans(int seg[],int ql,int qh,int l,int h,int pos){
+    if(ql>h || l>qh)return 1000;
+    if(ql<l && qh>=h)return seg[pos];
+    ll m=(l+h)/2;
+    return min(get_ans(seg,ql,qh,l,m,2*pos+1),get_ans(seg,ql,qh,m+1,h,2*pos+2));
+}
 int ans_query(vector<int>seg,int qlow,int qhigh,int low,int high,int pos){
     if(qlow<=low && qhigh>=high){
         return seg[pos];
@@ -29,10 +45,9 @@ int main() {
     int n;cin>>n;
       vector<int>a(n);
      for(int i;i<n;i++)cin>>a[i];
-    vector<int>seg(2*n-1);
+    vector<int>seg(7);
     const_tree(a,seg,0,n-1,0);
-    cout<<ans_query(seg,0,3,0,n-1,0)<<endl;
+    //cout<<ans_query(seg,1,3,0,n-1,0);
     for(int i=0;i<7;i++)cout<<seg[i]<<" ";
-
     return 0;
 }
--- Notes/Segment_Tree.md
+++ Notes/Segment_Tree.md
@@ -3,5 +3,13 @@
  ### so that both operations work in O(log n) time.
 # Hint on Size of segement tree Array
 ### If the size of the array is not a power of two, we can always append extra elements to it.
+# Finding subsegments with the maximal sum :
+### This time we will store four values for each vertex: 
+### [ the sum of the segment, the maximum prefix sum, the maximum suffix sum, and the sum of the maximal subsegment in it ]
+## -the answer of the left child, which means that the optimal subsegment is entirely placed in the segment of the left child
+ ## -the answer of the right child, which means that the optimal subsegment is entirely placed in the segment of the right child
+## -the sum of the maximum suffix sum of the left child and the maximum prefix sum of the right child, which means that the optimal subsegment intersects with both children.
+
 
 # https://www.youtube.com/watch?v=ZBHKZF5w4YU&t=1396s
+# https://cp-algorithms.com/data_structures/segment_tree.html
